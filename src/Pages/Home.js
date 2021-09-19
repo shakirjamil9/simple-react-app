ced4d9e41a86abb1dcd8ef0da12372f0f79d23fb
M Shakir Jamil <shakir@Ms-MacBook-Pro.local>
2021-09-19 13:37:27 +0500

    19 Sep

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,38 +2,47 @@ import React from "react";
 import MyCarousel from "../Components/MyCarousel"
 import axios from "axios";
 import img from "../download.png"
+import { Card, Button } from "react-bootstrap"
 
 const CardComponent = (props) => {
     const { cardData } = props;
     return (
-        <div className="card-cover">
-            <small>{cardData.id}</small> <br />
-            <img src={cardData.thumbnailUrl} alt=""/>
-            <h4>{cardData.title}</h4>
-        </div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={img} />
+            <Card.Body>
+                <Card.Title>{cardData.title}</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
     )
 }
 class Home extends React.Component {
     state = {
-        data: []
+        data: [],
+        loading: true
     }
 
     componentDidMount = async () => {
         const { data } = await axios.get('https://jsonplaceholder.typicode.com/photos')
         this.setState({
-            data: data
+            data: data,
+            loading: false
         })
     }
-
     render() {
-        console.log(this.state.data);
         return (
             <React.Fragment>
                 <MyCarousel />
                 <div className="card-container">
-                    {this.state.data.map((card, i) => {
-                        return <CardComponent cardData={card} index={i}/>
-                    })}
+                    {
+                        this.state.loading ? <div class="loader">Loading...</div> : this.state.data.slice(0, 100).map((card, i) => {
+                            return <CardComponent cardData={card} index={i} />
+                        })
+                    }
                 </div>
             </React.Fragment>
         )
